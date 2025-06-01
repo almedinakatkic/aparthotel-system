2ef062d0ab4cb92b0ab231d64d2f725eee7434f6
Ilma Makarevic <makarevic85@gmail.com>
2025-06-02 00:05:53 +0200

    Refactor SidebarHousekeeping component to use NavLink for navigation and improve layout

--- client/src/components/SidebarHousekeeping.js
+++ client/src/components/SidebarHousekeeping.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import '../assets/styles/sidebar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBroom } from '@fortawesome/free-solid-svg-icons';
 
 const SidebarHousekeeping = () => (
   <div className="sidebar">
-    <nav className="sidebar-menu">
-      <ul>
-        <li><Link to="/rooms">Cleaning Tasks</Link></li>
-      </ul>
-    </nav>
+    <div className="sidebar-content">
+      <nav className="sidebar-nav">
+        <NavLink 
+          to="/cleaning-tasks" 
+          className="nav-item"
+          activeClassName="active-nav-item"
+        >
+          <span className="nav-icon"><FontAwesomeIcon icon={faBroom} /></span>
+          <span className="nav-text">Cleaning Tasks</span>
+        </NavLink>
+        
+      </nav>
+    </div>
   </div>
 );
 
