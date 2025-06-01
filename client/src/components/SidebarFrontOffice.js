074b1aa1ce84ec7abf69349fecfc2060fc34946a
Ilma Makarevic <makarevic85@gmail.com>
2025-06-01 23:57:14 +0200

    Refactor SidebarFrontOffice component to use NavLink for navigation and enhance sidebar layout

--- client/src/components/SidebarFrontOffice.js
+++ client/src/components/SidebarFrontOffice.js
@@ -1,16 +1,45 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import '../assets/styles/sidebar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBellConcierge,
+  faUsers,
+  faBed
+} from '@fortawesome/free-solid-svg-icons';
 
 const SidebarFrontOffice = () => (
   <div className="sidebar">
-    <nav className="sidebar-menu">
-      <ul>
-        <li><Link to="/front-desk">Front Desk</Link></li>
-        <li><Link to="/guests">Guests</Link></li>
-        <li><Link to="/rooms">Rooms</Link></li>
-      </ul>
-    </nav>
+    <div className="sidebar-content">
+      <nav className="sidebar-nav">
+        <NavLink 
+          to="/front-desk" 
+          className="nav-item"
+          activeClassName="active-nav-item"
+        >
+          <span className="nav-icon"><FontAwesomeIcon icon={faBellConcierge} /></span>
+          <span className="nav-text">Front Desk</span>
+        </NavLink>
+
+        <NavLink 
+          to="/guests" 
+          className="nav-item"
+          activeClassName="active-nav-item"
+        >
+          <span className="nav-icon"><FontAwesomeIcon icon={faUsers} /></span>
+          <span className="nav-text">Guests</span>
+        </NavLink>
+
+        <NavLink 
+          to="/rooms" 
+          className="nav-item"
+          activeClassName="active-nav-item"
+        >
+          <span className="nav-icon"><FontAwesomeIcon icon={faBed} /></span>
+          <span className="nav-text">Rooms</span>
+        </NavLink>
+      </nav>
+    </div>
   </div>
 );
 
