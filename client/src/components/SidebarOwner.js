2dbefa36b68a8b6b20abdb656fbdf3bdbd6cfd72
Ilma Makarevic <makarevic85@gmail.com>
2025-06-02 00:09:16 +0200

    Refactor SidebarOwner component to use NavLink for navigation and improve layout

--- client/src/components/SidebarOwner.js
+++ client/src/components/SidebarOwner.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import '../assets/styles/sidebar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChartLine } from '@fortawesome/free-solid-svg-icons'; // Using chart icon for reports
 
 const SidebarOwner = () => (
   <div className="sidebar">
-    <nav className="sidebar-menu">
-      <ul>
-        <li><Link to="/owner-report">My Report</Link></li>
-      </ul>
-    </nav>
+    <div className="sidebar-content">
+      <nav className="sidebar-nav">
+        <NavLink 
+          to="/owner-report" 
+          className="nav-item"
+          activeClassName="active-nav-item"
+        >
+          <span className="nav-icon"><FontAwesomeIcon icon={faChartLine} /></span>
+          <span className="nav-text">My Report</span>
+        </NavLink>
+      </nav>
+    </div>
   </div>
 );
 
