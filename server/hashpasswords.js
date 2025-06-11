e3923c2fe7af92e31f3e957fc025556da1790863
Ilma Makarevic <makarevic85@gmail.com>
2025-06-11 22:21:11 +0200

    update .gitignore

--- server/hashpasswords.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-async function run() {
-  const password = 'test123'; 
-  const hash = await bcrypt.hash(password, 10);
-  console.log('Hashed password:', hash);
-}
-
-run();
